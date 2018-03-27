81b9082a698476415e4ebc43378fb275617caf3e
paul.jeanbour <pa.jeanbourquin@gmail.com>
2018-03-27 15:17:34 +0200

    Make some tests

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from Apply import apply_road_detection as detects
 from Provider import SequenceProvider, VideoProvider
-from Callback import SaveImageCallback
+from Callback import SaveImageCallback, TurtleCallback
 from OtsuBasedDetection import OtsuDetection
 
 
 def freescale():
-    detects(provider=SequenceProvider("../picture_freescale/15.04.16/Avant/Sequence6/"),
+    detects(provider=SequenceProvider("../picture_freescale/15.04.16/Avant/Sequence5/"),
             algorithm=OtsuDetection(),
-            callback=SaveImageCallback("output"),
+            # callback=SaveImageCallback("output"),
+            callback=TurtleCallback(),
             gui=True)
 
 
@@ -15,11 +16,11 @@ def gta():
     from Apply import Frequency
     detects(provider=VideoProvider("../gta.mp4"),
             algorithm=OtsuDetection(inverse=True),
-            callback=SaveImageCallback("GTA_output"),
+            # callback=SaveImageCallback("GTA_output"),
             gui=True,
             frequency=Frequency.HZ1000)
 
 
 if __name__ == "__main__":
-    freescale()
-    # gta()
+    #freescale()
+    gta()
